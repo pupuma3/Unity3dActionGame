0293ef3bfa778e752fcb6904f5e418b0a1becbbf
pupuma3 <oukie3@gmail.com>
2018-03-10 17:33:50 +0900

    ADD { AnimationModule.cs / and AttackState.cs }

---
+++ Assets/01.Scripts/AnimationModule.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationModule : MonoBehaviour
+{
+    // Animation
+    System.Action _startCallback = null;
+    System.Action _middleCallback = null;
+    System.Action _endCallback = null;
+    public void Play(string triggerName, System.Action startCallback, System.Action middleCallback,
+        System.Action endCallback)
+    {
+        gameObject.GetComponent<Animator>().SetTrigger(triggerName);
+        _startCallback = startCallback;
+        _middleCallback = middleCallback;
+        _endCallback = endCallback;
+    }
+    public void OnStartAnimation()
+    {
+        if (null != _startCallback)
+        {
+            _startCallback();
+        }
+    }
+    public void OnMiddleAnimation()
+    {
+        if (null != _middleCallback)
+        {
+            _middleCallback();
+        }
+    }
+    public void OnEndAnimation()
+    {
+        if(null !=_endCallback)
+        {
+            _endCallback();
+        }
+    }
+}
